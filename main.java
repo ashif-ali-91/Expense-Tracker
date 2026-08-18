9fc0fd058fb993946a96b68744dc0a8cb131112e
ASHIF ALI <ashif12666@gmail.com>
2026-08-17 21:36:47 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,71 @@
+package com.example.expensetracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private double amount;
+
+    private String category;
+
+    private String date;
+
+    public Expense() {
+    }
+
+    public Expense(
+            String title,
+            double amount,
+            String category,
+            String date) {
+
+        this.title = title;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
